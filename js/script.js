40759b1fce0800afa7af798488408e64606165f7
davidjoaq <118952873+davidsosa2022@users.noreply.github.com>
2023-03-02 15:50:15 -0500

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,6 @@
+vino.ir_enableCodeset(1);
+const activeUserSlot = vino.act_getCurrentSlotNo(); // The slot for the active user
+img = document.getElementById("mii-image");
+span = document.getElementById("mii-name");
+img.src=vino.act_getMiiImage(activeUserSlot);
+span.innerText=vino.act_getName(activeUserSlot);

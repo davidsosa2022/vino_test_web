26b02e14147f6b0a02d6405e6b8915b83426838b
davidjoaq <118952873+davidsosa2022@users.noreply.github.com>
2023-03-16 20:34:44 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
+var loader;
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.05);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
+
+window.addEventListener("load", function () {
+          if (!sessionStorage.viewed){
+              const loader = document.querySelector(".loader");
+              loader.className += " hidden";
+              sessionStorage.viewed  = 1;
+          }else{
+            const loader = document.querySelector(".loader");
+            loader.style.display = "none";
+          }
+});
+
 vino.ir_enableCodeset(1);
 const activeUserSlot = vino.act_getCurrentSlotNo(); // The slot for the active user
 img = document.getElementById("mii-image");

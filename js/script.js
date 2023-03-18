fd162350cb032317e7c2945e0e19bc6c29c8b7cb
davidjoaq <118952873+davidsosa2022@users.noreply.github.com>
2023-03-17 20:04:16 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,40 +1,56 @@
-var loader;
-
-function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-    } else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function() {
-            loadNow(opacity - 0.05);
-        }, 50);
-    }
-}
-
-function displayContent() {
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    loader = document.getElementById('loader');
-    loadNow(1);
-});
-
-window.addEventListener("load", function () {
-          if (!sessionStorage.viewed){
-              const loader = document.querySelector(".loader");
-              loader.className += " hidden";
-              sessionStorage.viewed  = 1;
-          }else{
-            const loader = document.querySelector(".loader");
-            loader.style.display = "none";
-          }
-});
-
-vino.ir_enableCodeset(1);
 const activeUserSlot = vino.act_getCurrentSlotNo(); // The slot for the active user
 img = document.getElementById("mii-image");
 span = document.getElementById("mii-name");
 img.src=vino.act_getMiiImage(activeUserSlot);
 span.innerText=vino.act_getName(activeUserSlot);
+
+// Abstracted IR codes
+const IR_CODES = {
+    POWER: 0,
+    CHANNEL_UP: 1,
+    CHANNEL_DOWN: 2,
+    VOLUME_UP: 3,
+    VOLUME_DOWN: 4,
+    NUMBER_ONE: 11,
+    NUMBER_TWO: 12,
+    NUMBER_THREE: 13,
+    NUMBER_FOUR: 14,
+    NUMBER_FIVE: 15,
+    NUMBER_SIX: 16,
+    NUMBER_SEVEN: 17,
+    NUMBER_EIGHT: 18,
+    NUMBER_NINE: 19,
+    NUMBER_ZERO: 20,
+    INPUT: 52
+};
+
+vino.ir_enableCodeset(1); // Enable IR protocol code set
+
+// Helper functions
+function toggleTVPower() {
+    vino.ir_send(IR_CODES.POWER, 0);
+}
+
+function changeInput() {
+    vino.ir_send(IR_CODES.INPUT, 0);
+}
+
+function increaseVolume() {
+    vino.ir_send(IR_CODES.VOLUME_UP, 0);
+}
+
+function decreaseVolume() {
+    vino.ir_send(IR_CODES.VOLUME_DOWN, 0);
+}
+
+function increaseChannel() {
+    vino.ir_send(IR_CODES.CHANNEL_UP, 0);
+}
+
+function decreaseChannel() {
+    vino.ir_send(IR_CODES.CHANNEL_DOWN, 0);
+}
+
+function muteSound() {
+    vino.ir_muteOneShotSound(true);
+}

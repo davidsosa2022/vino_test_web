e05f66f74f9b18572a6cc19b27a5e0b8040c7526
davidjoaq <118952873+davidsosa2022@users.noreply.github.com>
2023-03-20 14:23:47 -0500

    Delete preloader.js

--- js/preloader.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function($) {
-    var Body = $('body');
-    Body.addClass('preloader-site');
-});
-$(window).load(function() {
-    $('.preloader-wrapper').fadeOut();
-    $('body').removeClass('preloader-site');
-});

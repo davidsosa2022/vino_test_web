934d27ee1685253440571a21d4f8b092afd75dbb
davidjoaq <118952873+davidsosa2022@users.noreply.github.com>
2023-03-22 19:10:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ const activeUserSlot = vino.act_getCurrentSlotNo();
 span = document.getElementById("mii-name");
 span.innerText=vino.act_getName(activeUserSlot);
 img = document.getElementById("mii-image");
-img.src=vino.act_getMiiBody(activeUserSlot);
+img.src=vino.act_getMiiImage(activeUserSlot);
 
 
 

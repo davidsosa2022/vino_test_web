14256033bb00fc0382fcbf1a7fcea7b8a3d978f4
Parker <51893650+Parkeriscoo@users.noreply.github.com>
2023-03-23 20:01:02 -0500

    Create swap.js

---
+++ alerts/swap.js
@@ -0,0 +1,9 @@
+ <script>
+  if (confirm("Are you sure you want to switch to the TVii Release version?")){
+ window.location.href = "http://vino.latteu.net/";
+}
+else
+{
+    window.location.href = "./tags.html";
+}
+  </script>
